21db24843fd953946b492fcb2737fa75339c6c93
seinnletlethninn <sllhninn.6@gmail.com>
2021-12-12 02:12:51 +0630

    added program

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,7 +1,8 @@
 <?php
   
-  // custom event
   function university_post_types() {
+    
+    // event post type
     register_post_type( 'event', array(
       'supports' => array('title', 'editor', 'excerpt'),
       'rewrite' => array('slug' => 'events'),
@@ -16,7 +17,24 @@
         'singular_name' => 'Event'
       ), 
       'menu_icon' => 'dashicons-calendar'
-    ) );
+    ));
+
+    // program post type
+    register_post_type( 'program', array(
+      'supports' => array('title', 'editor'),
+      'rewrite' => array('slug' => 'programs'),
+      'has_archive' => true,
+      'public' => true,
+      'show_in_rest' => true,
+      'labels' => array(
+        'name' => 'Programs',
+        'add_new_item' => 'Add New Program',
+        'edit_item' => 'Edit Program',
+        'all_items' => 'All Programs',
+        'singular_name' => 'Program'
+      ), 
+      'menu_icon' => 'dashicons-awards'
+    ));
   }
   add_action( 'init', 'university_post_types' );
 
--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -22,7 +22,14 @@
 
   //manipulating default url based queries
   function university_adjust_queries($query) {
-    if(!is_admin() && is_post_type_archive( 'event' ) && $query->is_main_query()) {
+
+    if(!is_admin() && is_post_type_archive( 'program' ) && is_main_query()) {
+      $query->set('orderby', 'title');
+      $query->set('order', 'ASC');
+      $query->set('posts_per_page', -1);
+    }
+
+    if(!is_admin() && is_post_type_archive( 'event' ) && is_main_query()) {
       
       $today = date('Ymd');
       
