3df397af8cb987792ff840af90963cb6aacfd6b3
seinnletlethninn <sllhninn.6@gmail.com>
2021-12-15 14:36:30 +0630

    added traditional search

---
+++ wp-content/themes/fictional-university-theme/searchform.php
@@ -0,0 +1,7 @@
+<form class="search-form" action="<?php echo esc_url(site_url('/')); ?>" method="get">
+  <label class="headline headline--medium" for="s">Perform a New Search:</label>
+  <div class="search-form-row">
+    <input class="s" placeholder="What are you looking for?" type="search" name="s" id="s" autofocus/>
+    <input class="search-submit" type="submit" value="Search" />
+  </div>
+</form>
